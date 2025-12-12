704cdfb666d2323fe9e8e3055864a629f2308889
mhhidayat <mhhidayat811@gmail.com>
2025-12-13 00:03:28 +0700

    refactor: webhoook discord interface

--- src/Interface/DiscordWebhookInterface.php
+++ src/Interface/DiscordWebhookInterface.php
@@ -3,21 +3,20 @@
 namespace Mhhidayat\PhpDiscordClient\Interface;
 
 use Closure;
-use Mhhidayat\PhpDiscordClient\Core\MainDiscordClient;
 
 interface DiscordWebhookInterface {
-    public static function make(): MainDiscordClient;
-    public function setWebhookURL(string $setWebhookURL): MainDiscordClient;
-    public function setContent(array|Closure $contentHandler): MainDiscordClient;
-    public function addEmbeds(Closure $embedsHandler): MainDiscordClient;
-    public function text(string $text): MainDiscordClient;
-    public function setUsername(string $username): MainDiscordClient;
-    public function setAvatar(string $avatarURL): MainDiscordClient;
-    public function allowTTS(): MainDiscordClient;
-    public static function withHeaders(array $headers): MainDiscordClient;
-    public static function timeout(int $seconds): MainDiscordClient;
-    public function send(): MainDiscordClient;
-    public function sendWhen(bool|Closure $isSendHandler): MainDiscordClient;
+    public static function make(): self;
+    public function setWebhookURL(string $setWebhookURL): self;
+    public function setContent(array|Closure $contentHandler): self;
+    public function addEmbeds(Closure $embedsHandler): self;
+    public function text(string $text): self;
+    public function setUsername(string $username): self;
+    public function setAvatar(string $avatarURL): self;
+    public function allowTTS(): self;
+    public static function withHeaders(array $headers): self;
+    public static function timeout(int $seconds): self;
+    public function send(): self;
+    public function sendWhen(bool|Closure $isSendHandler): self;
     public function successful(): bool;
     public function failed(): bool;
     public function getResponseJson(): string;
