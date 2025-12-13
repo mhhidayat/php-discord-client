f9138e64c572324150a026f653b713281f8f904d
mhhidayat <mhhidayat811@gmail.com>
2025-12-13 18:52:53 +0700

    feat: add discord bot interface

---
+++ src/Interface/DiscordBotInterface.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace Mhhidayat\PhpDiscordClient\Interface;
+
+use Closure;
+
+interface DiscordBotInterface {
+    public static function make(): self;
+    public static function withHeaders(array $headers): self;
+    public static function timeout(int $seconds): self;
+    public static function withConfig(array $config): self;
+    public function setWebhookURL(string $setWebhookURL): self;
+    public function setContent(array|Closure $contentHandler): self;
+    public function addEmbeds(Closure $embedsHandler): self;
+    public function text(string $text): self;
+    public function setUsername(string $username): self;
+    public function setAvatar(string $avatarURL): self;
+    public function allowTTS(): self;
+    public function send(): self;
+    public function sendWhen(bool|Closure $isSendHandler): self;
+    public function successful(): bool;
+    public function failed(): bool;
+    public function getResponseJson(): string;
+}
