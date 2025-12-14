1dc0f603dd2889a8759b635ae6bbfc1a81484968
mhhidayat <mhhidayat811@gmail.com>
2025-12-14 18:11:42 +0700

    feat: change http client call

--- src/Core/CoreDiscordClient.php
+++ src/Core/CoreDiscordClient.php
@@ -2,6 +2,8 @@
 
 namespace Mhhidayat\PhpDiscordClient\Core;
 
+use Mhhidayat\PhpDiscordClient\DiscordBot;
+use Mhhidayat\PhpDiscordClient\DiscordWebhook;
 use Mhhidayat\PhpDiscordClient\Exception\DiscordClientException;
 use Mhhidayat\PhpDiscordClient\Http\HttpClient;
 use Mhhidayat\PhpDiscordClient\Http\HttpResponse;
@@ -12,19 +14,39 @@ class CoreDiscordClient
     protected string $text = "";
     protected string $username = "";
     protected string $avatarURL = "";
+    protected string $channelID = "";
+    protected string $botToken = "";
     protected array $content = [];
     protected array $embeds = [];
+    protected array $headers = [];
     protected bool $allowTTS = false;
+    protected int $timeout = 15;
     protected ?HttpResponse $lastResponse = null;
-    protected HttpClient $httpClient;
+    protected string $discordClientTarget;
 
     public function __construct(array $headers = [], int $timeout = 15)
     {
-        $this->httpClient = new HttpClient($headers, $timeout);
+        $this->headers = $headers;
+        $this->timeout = $timeout;
+        $this->getDiscordClientTarget();
+    }
+
+    public function getDiscordClientTarget(): void
+    {
+        $this->discordClientTarget = match(static::class) {
+            DiscordBot::class => "bot",
+            DiscordWebhook::class => "webhook",
+            default => throw new DiscordClientException("Class name unknown.")
+        };
     }
 
     protected function getURL(): string
     {
+        if ($this->discordClientTarget == "bot") {
+            $channelID = $this->channelID;
+            return "https://discord.com/api/v10/channels/{$channelID}/messages";
+        }
+
         if (empty($this->webhookURL)) {
             throw new DiscordClientException(
                 "Webhook URL is not set. Use the setWebhookURL() method to set it."
@@ -64,11 +86,28 @@ class CoreDiscordClient
         return $payload;
     }
 
+    protected function getHeaders(): array
+    {
+        $headers = $this->headers;
+
+        if (!in_array("Content-Type: application/json", $headers)) {
+            array_push($headers, "Content-Type: application/json");
+        }
+
+        if ($this->discordClientTarget == "bot") {
+            array_push($headers, "Authorization: Bot {$this->botToken}");
+        }
+
+        return $headers;
+    }
+
     protected function sendRequest(): void
     {
         $url = $this->getURL();
+        $headers = $this->getHeaders();
         $payload = json_encode($this->buildPayload());
         
-        $this->lastResponse = $this->httpClient->post($url, $payload);
+        $this->lastResponse = (new HttpClient($headers, $this->timeout))
+            ->post($url, $payload);
     }
 }
