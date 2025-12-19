7f805df0bc7c7f7801220710c9419740264ef90a
mhhidayat <mhhidayat811@gmail.com>
2025-12-19 23:42:27 +0700

    feat: add name function

--- src/Contract/EmbedsFieldsContract.php
+++ src/Contract/EmbedsFieldsContract.php
@@ -9,6 +9,16 @@ class EmbedsFieldsContract implements ContractInterface
     private array $fieldsData = [];
     private int $currentIndex = -1;
 
+    public function name(string $name): self
+    {
+        $this->fieldsData[] = [
+            'name' => $name,
+        ];
+
+        $this->currentIndex = array_key_last($this->fieldsData);
+        return $this;
+    }
+
     public function build(): array
     {
         return $this->fieldsData;
